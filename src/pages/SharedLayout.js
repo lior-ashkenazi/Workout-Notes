87f39aa665cfa8c381551be6a5d2bfba274be2d7
Lior Ashkenazi <lior.ashkenazi>
2023-01-10 21:50:58 +0200

    Background was added

--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -3,7 +3,7 @@ import Sidebar from "../components/Sidebar";
 
 export default function SharedLayout() {
   return (
-    <div className="screen flex h-screen w-full bg-purple-100 font-roboto">
+    <div className="screen flex bg-img h-screen w-full 0 font-roboto">
       <Sidebar />
       <Outlet />
     </div>
