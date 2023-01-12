0428753c55dd242daeb0627dfaf474c50eeb8138
Lior Ashkenazi <lior.ashkenazi>
2023-01-12 17:27:35 +0200

    Hotfix for deleting a sidebar-item and not returning to home-page

--- src/components/sidebar/SidebarButton.js
+++ src/components/sidebar/SidebarButton.js
@@ -35,8 +35,8 @@ export default function SidebarButton({
       <span className="absolute opacity-0 group-hover:opacity-100 transition-opacity duration-300 transform top-0 right-0 mt-2.5 mr-2">
         {deleteClicked ? (
           <>
-            <Link to="/Workout-Notes">
-              <button className="" onClick={onDelete}>
+            <Link to="/Workout-Notes/">
+              <button onClick={onDelete}>
                 <HiCheck />
               </button>
             </Link>
