1b30439a6d10b066fc39d236d16fa4b6328041ba
Lior Ashkenazi <lior.ashkenazi>
2023-01-12 23:06:27 +0200

    Hotfix for refreshing bug v3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 import SharedLayout from "./pages/SharedLayout";
 import HomePage from "./pages/HomePage";
 import WorkoutPage from "./pages/WorkoutPage";
@@ -6,7 +6,7 @@ import ErrorPage from "./pages/ErrorPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <MemoryRouter initialEntries={["/"]}>
       <Routes>
         <Route path="/Workout-Notes" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
@@ -14,6 +14,6 @@ export default function App() {
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </MemoryRouter>
   );
 }
